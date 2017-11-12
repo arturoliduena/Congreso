b1ad0ad395b7aeacb2ce6ad45a29e92afb5ce849
Arturo <arturo.liduena@gmail.com>
2017-11-12 13:38:21 -0300

    add Map without Marker

--- Components/map.js
+++ Components/map.js
@@ -1,25 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: null,
+        longitude: null,
+        latitudeDelta: null,
+        longitudeDelta: null,
+      },
+      latitude: null,
+      longitude: null,
+      error: null,
+      location: false,
+      animating: true
+    };
+  };
+
   static navigationOptions = {
     title: 'map',
   }
 
+  componentWillMount(){
+    navigator.geolocation.getCurrentPosition(
+      (geo_success) => {
+        this.setState({
+          latitude: geo_success.coords.latitude,
+          longitude: geo_success.coords.longitude,
+          region: {
+            latitude: geo_success.coords.latitude,
+            longitude: geo_success.coords.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          },
+          error: null,
+          location: true,
+          animating: false
+        });
+        console.log('setState', this.state)
+      },
+      (geo_error) =>
+      this.setState({ error: geo_error })
+      ,
+      { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 },
+    );
+  
+    }
+  
+
   render() {
+    if (!this.state.location) 
+    return (
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+          <ActivityIndicator 
+            animating= {this.state.animating}
+          />
+        </View>
+        )
     return (
       <View style={styles.container}>
-        <Text>Este es el mapa!!</Text>
+        <MapView
+          style={styles.map}
+          region={this.state.region}
+        >
+          <Marker
+          coordinate={{latitude: this.state.latitude ,longitude: this.state.longitude}}
+          title='You are here'
+          >
+            <View style= {styles.marker} />
+          </Marker>
+
+
+        </MapView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  marker:{
+    height: 25,
+    width: 25,
+    borderWidth: 3,
+    borderColor: "white",
+    borderRadius: 25 / 2,
+    overflow: "hidden",
+    backgroundColor: "rgba(0,122,255,0.8)",
+
+  },
   container: {
+    ...StyleSheet.absoluteFillObject,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
